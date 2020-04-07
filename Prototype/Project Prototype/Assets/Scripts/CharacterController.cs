9b0c09134fe77b617f84c1d0bed9a62327e633c0
adalferrer <adalgisaferrer@gmail.com>
2020-04-06 21:37:30 -0400

    intro in progress

---
+++ Prototype/Project Prototype/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+      
+        Debug.Log(anim);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
