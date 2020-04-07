18c0a50b0e935784982d11961c7ac965b0675c99
carreath <carreath@icloud.com>
2020-04-07 17:15:08 -0300

    Added Bouncy Wall Type

--- Prototype/CS2053-Project/Assets/Scripts/LevelCreation/Objects/Wall.cs
+++ Prototype/CS2053-Project/Assets/Scripts/LevelCreation/Objects/Wall.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class Wall : MapObject
 {
+    public enum WallType {
+        Default,
+        Bouncy
+    }
+    public WallType type;
     public Position transform;
+
+    public Wall(int xGrid1, int zGrid1, int xGrid2, int zGrid2, WallType type) {
+        setupWall(xGrid1, zGrid1, xGrid2, zGrid2);
+        this.type = type;
+    }
     public Wall(int xGrid1, int zGrid1, int xGrid2, int zGrid2) {
-        
+        setupWall(xGrid1, zGrid1, xGrid2, zGrid2);
+        this.type = WallType.Default;
+    }   
+
+    public void setupWall(int xGrid1, int zGrid1, int xGrid2, int zGrid2) {
         float xScale = (xGrid2 - xGrid1) / 2f;
         if (xGrid1 == xGrid2) {
             xScale = 0.5f;
