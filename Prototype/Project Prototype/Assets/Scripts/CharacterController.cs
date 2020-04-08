bd46ed9fd51dc11f3fdadfa75edfdfeed48531a3
adalferrer <adalgisaferrer@gmail.com>
2020-04-08 08:45:05 -0400

    Intro and Outro added

--- Prototype/Project Prototype/Assets/Scripts/CharacterController.cs
+++ Prototype/Project Prototype/Assets/Scripts/CharacterController.cs
@@ -4,17 +4,37 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    public Dialog dialog;
+    private Material mat;
+
+    GameObject cage; 
+
     private Animator anim;
+    private string currentName;
+    private bool first;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
-      
-        Debug.Log(anim);
+        cage = GameObject.FindGameObjectWithTag("Cage");
+        first = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentName = dialog.GetName();
+
+        if(currentName == gameObject.name)
+        {
+            anim.Play("Floating");
+        }
+        else
+        {
+            anim.Play("Empty");
+        }
     }
+
 }
