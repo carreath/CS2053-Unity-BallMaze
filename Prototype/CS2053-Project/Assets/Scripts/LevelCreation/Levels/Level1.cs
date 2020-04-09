0dfa65cc58afeef98bfe8528543d354506c6ffac
Moses Cruz <mcruz@Mosess-MacBook-Pro.local>
2020-04-08 22:04:49 -0300

    added wall to check where our gits are at

--- Prototype/CS2053-Project/Assets/Scripts/LevelCreation/Levels/Level1.cs
+++ Prototype/CS2053-Project/Assets/Scripts/LevelCreation/Levels/Level1.cs
@@ -27,6 +27,7 @@ public class Level1 : Map
 
             new Wall(18, 5, 19, 20),
             new Wall(18, 9, 17, 15),
+            new Wall(18, 9, 17, 15),
             
         };
 
@@ -45,19 +46,19 @@ public class Level1 : Map
             
         };
         introDialogue = new Dialogue[] {
-            new Dialogue(9, Dialogue.Speaker.Rubik, "Welcome To your doom!"),
-            new Dialogue(5, Dialogue.Speaker.Rubik, "We're going to have some fun you and I!"),
-            new Dialogue(5, Dialogue.Speaker.Rubik, "Because now the walls turn against you! You thought you could find solace against these edges, but now they will push you to your sanity’s edge! HAHAHA!"),
+            new Dialogue(90, Dialogue.Speaker.Rubik, "Welcome To your doom!"),
+            new Dialogue(50, Dialogue.Speaker.Rubik, "We're going to have some fun you and I!"),
+            new Dialogue(50, Dialogue.Speaker.Rubik, "Because now the walls turn against you! You thought you could find solace against these edges, but now they will push you to your sanity’s edge! HAHAHA!"),
 
         };
         outroDialogue = new Dialogue[] {
-            new Dialogue(5, Dialogue.Speaker.Rubik, "I have more planned for you, you just wait and see. You'll crack like Humpty Dumpty did in our maze!"),
+            new Dialogue(50, Dialogue.Speaker.Rubik, "I have more planned for you, you just wait and see. You'll crack like Humpty Dumpty did in our maze!"),
         };
 
         failDialogue = new Dialogue[] {
-            new Dialogue(5, Dialogue.Speaker.King, "Hey Rubik, I have a great new idea! You ever try to fit a round peg in a square hole?"),
-            new Dialogue(5, Dialogue.Speaker.Rubik, "I think I know where you're pointing to! I gotta say, it sounds FITTING!"),
-            new Dialogue(5, Dialogue.Speaker.King, "Well, thank cube very much! Now get to it!"),
+            new Dialogue(50, Dialogue.Speaker.King, "Hey Rubik, I have a great new idea! You ever try to fit a round peg in a square hole?"),
+            new Dialogue(50, Dialogue.Speaker.Rubik, "I think I know where you're pointing to! I gotta say, it sounds FITTING!"),
+            new Dialogue(50, Dialogue.Speaker.King, "Well, thank cube very much! Now get to it!"),
 
         };
     
